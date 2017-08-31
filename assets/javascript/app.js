d48295f006b48197a2fc35ea2ab0fb5e478a912c
myskam7 <mksmflppv98@gmail.com>
2017-08-31 09:54:47 -0400

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,147 @@
-var QA = [
-    { question1: "What is the largest ocean?",
-      correctA: "Pacific Ocean",
-      wrongA1: "Atlantic Ocean",
-      wrongA2: "Red Sea",
-      wrongA3: "Gulf of Mexico"
-
-    },
-
-        { question2: ("What is the capital of New York?"),
-        correctA: "Albany",
-        wrongA1: "New York City",
-        wrongA2: "New Jersey",
-        wrongA3: "Albania",
-        secondQ: function(){
-            $(this).on("click")
-        }
-        
-
-    }
-
-
-
-];
-
-// console.log(QA[0].question);
-// console.log(QA[0].correctA);
-
-$(".question").append("<h2>"+ QA[0].question1 +"</h2>");
-$(".answer").append("<h3>"+ QA[0].correctA +"</h3>");
-$(".answer").append("<h3>"+ QA[0].wrongA1 +"</h3>");
-$(".answer").append("<h3>"+ QA[0].wrongA2 +"</h3>");
-$(".answer").append("<h3>"+ QA[0].wrongA3 +"</h3>");
 
 
 
 
 
+var questionArray =  
+                    ["In what year was LEGO found?", 
+                    "What does LEGO stand for?", 
+                    "Which country did LEGO originate from?", 
+                    "Which is largest commercially produced LEGO set?",
+                    "Currently Millau Viaduct is the world's tallest bridge, what type of bridge is it?"];
+
+var answerArray =   
+                    [["1952", "1943", "1958", "1932"],
+                    ["LEt's GO", "LEg GOdt", "Nothing", "LE GOûter"],
+                    ["Belgium", "Denmark", "Switzarland", "Sweden"],
+                    ["Taj Mahal", "Death Star", "Grand Carousel", "Death Star II"],
+                    ["Suspension", "Cable-stayed", "Concrete", "Cable-suspension"]];
+var correctAnswers = 
+                     ["D. 1932", "B. LEg GOdt", "B. Denmark", "A. Taj Mahal", "B. Cable-stayed"];
+
+var questionCounter = 0;
+var selectedAnswer = 0;
+var theClock;
+var correctTally = 0; 
+var incorrectTally = 0;
+var unansweredTally = 0;
+var startScreen; 
+var gameHTML;
+var counter = 30;
 
 
 
-// $(document).ready(function () {
 
 
-//     function page(question, rightAnswer) {
-//         this.question = question;
-//         this.rightAnswer = rightAnswer;
-
-//     }
-
-
-//     var page1 = new page("What is his name?", "Doe");
+$(document).ready(function() {
     
-
-
-
-
-//     function check(px) {
-
-//         $.each(px, function (key, value) {
-
-//             var x = value;
-
-
-//             if (key ===  "question") {
-
-//                 $(".question").append("<h3>" + value + "</h3>")
-
-//             } else {
-//                 $(".answer").append("<h2>" + value + "</h2>")
-//             }
-
-
-
-//         })
-
-//         $("h2").on("click", function () {
-
-//             if (page1.rightAnswer) {
-
-//             }
-//         })
+    
+    function initialScreen() {
+        startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+        $(".mainArea").html(startScreen);
+    }
+    
+    initialScreen();
+    
+   
+    
+    $("body").on("click", ".start-button", function(event){
+        event.preventDefault();  
        
-
-//     }
-
-//     check(page1);
-
-
-// });
+        generateHTML();
+    
+        timerWrapper();
+    
+    }); 
+    
+    $("body").on("click", ".answer", function(event){
+        //answeredQuestion = true;
+       
+        selectedAnswer = $(this).text();
+        if(selectedAnswer === correctAnswers[questionCounter]) {
+           
+    
+            clearInterval(theClock);
+            generateWin();
+        }
+        else {
+           
+            clearInterval(theClock);
+            generateLoss();
+        }
+    }); 
+    
+    $("body").on("click", ".reset-button", function(event){
+       
+        resetGame();
+    }); 
+    
+    });  //  Closes jQuery wrapper
+    
+    function generateLossDueToTimeOut() {
+        unansweredTally++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>";
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 4000);  
+    }
+    
+    function generateWin() {
+        correctTally++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 4000);  
+    }
+    
+    function generateLoss() {
+        incorrectTally++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" ;
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 4000); 
+    }
+    
+    function generateHTML() {
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+        $(".mainArea").html(gameHTML);
+    }
+    
+    function wait() {
+        if (questionCounter < 4) {
+        questionCounter++;
+        generateHTML();
+        counter = 30;
+        timerWrapper();
+        }
+        else {
+            finalScreen();
+        }
+    }
+    
+    function timerWrapper() {
+        theClock = setInterval(thirtySeconds, 1000);
+        function thirtySeconds() {
+            if (counter === 0) {
+                clearInterval(theClock);
+                generateLossDueToTimeOut();
+            }
+            if (counter > 0) {
+                counter--;
+            }
+            $(".timer").html(counter);
+        }
+    }
+    
+    function finalScreen() {
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+        $(".mainArea").html(gameHTML);
+    }
+    
+    function resetGame() {
+        questionCounter = 0;
+        correctTally = 0;
+        incorrectTally = 0;
+        unansweredTally = 0;
+        counter = 30;
+        generateHTML();
+        timerWrapper();
+    }
+    
